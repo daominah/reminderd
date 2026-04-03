9cefe9c86718ad1c43242068ac5d3d9bd22e849d
TungDao <tung.daothanhtung@gmail.com>
2026-04-03 17:40:57 +0700

    add notify tests, fix AppleScript injection when title or message contains quotes

--- pkg/driver/notify/notify_darwin.go
+++ pkg/driver/notify/notify_darwin.go
@@ -1,11 +1,22 @@
 package notify
 
-import "os/exec"
+import (
+	"os/exec"
+	"strings"
+)
 
 // OSNotifier sends notifications via macOS osascript.
 type OSNotifier struct{}
 
 func (n *OSNotifier) Notify(title, message string) error {
+	title = escapeAppleScript(title)
+	message = escapeAppleScript(message)
 	script := `display notification "` + message + `" with title "` + title + `"`
 	return exec.Command("osascript", "-e", script).Run()
+}
+
+func escapeAppleScript(s string) string {
+	s = strings.ReplaceAll(s, `\`, `\\`)
+	s = strings.ReplaceAll(s, `"`, `\"`)
+	return s
 }
